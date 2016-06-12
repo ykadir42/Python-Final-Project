703a1df33403ac67bf4dfd6e15a0aa3726a722ef
ykadir42 <ykadir42@gmail.com>
2016-06-12 18:49:59 +0000

    Add files via upload

---
+++ csvToDict.py
@@ -0,0 +1,32 @@
+print '0. csvToDict'
+
+def boolean(string):
+    if string=="True":
+        return True
+    elif string=="False":
+        return False
+    else:
+        return string
+
+def csvToDict (fileName): #Will produce a dictionary
+    source = open( fileName, 'rU')
+    content = source.read()
+    source.close()
+    listOfLines = content.split('\n')
+    titles=listOfLines.pop(0).split(',')
+    output={}
+    for line in listOfLines:
+        elementsList = line.split(',')
+        pos=1
+        curDict={}
+        while pos<len(elementsList):
+            try:
+                curDict[titles[pos]]=float(elementsList[pos])
+            except ValueError:
+                curDict[titles[pos]]=boolean(elementsList[pos])
+            pos+=1
+        output[ elementsList[0]] = curDict
+    output["titles"]=titles
+    return output
+
+print csvToDict("sessions.csv")
