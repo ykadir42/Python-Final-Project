4e1cc6149f9278de686c93487fb4dae12f0d266f
ykadir42 <ykadir42@gmail.com>
2016-06-13 00:11:56 -0400

    Add files via upload

--- addAccount.py
+++ addAccount.py
@@ -4,5 +4,5 @@ dataDict=csvToDict.csvToDict('sessions.csv')
 
 def addAccount(sessID,password):
     dest=open('sessions.csv','a',0)
-    dest.write("\n"+sessID+","+password+","*(len(dataDict['titles'])-2))
+    dest.write("\n"+sessID+","+password+",0,"*(len(dataDict['titles'])-3))
     dest.close
