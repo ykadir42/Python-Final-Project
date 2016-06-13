c1f42a4ccd4dd6c0bf87a0873a91821239306300
ykadir42 <ykadir42@gmail.com>
2016-06-13 01:48:46 -0400

    Add files via upload

--- addAccount.py
+++ addAccount.py
@@ -4,5 +4,5 @@ dataDict=csvToDict.csvToDict('sessions.csv')
 
 def addAccount(sessID,password):
     dest=open('sessions.csv','a',0)
-    dest.write("\n"+sessID+","+password+",0"+","*(len(dataDict['titles'])-3))
+    dest.write("\n"+sessID+","+password+",0,False"+","*(len(dataDict['titles'])-3))
     dest.close
--- game.py
+++ game.py
@@ -14,6 +14,7 @@ import addAccount
 print 'content-type: text/html\n'
 
 sessionData=csvToDict.csvToDict('sessions.csv')
+playerList=['0','1','2','3']
 sessID=fromClient['sessID'].value
 hiddenForm=htmlFunctions.element(False,1,"input",'type="hidden" name="sessID" value="'+sessID+'"','')
 player=sessionData[sessID]['player']
@@ -22,6 +23,8 @@ def nextPlayer():
         return 0
     else:
         return player+1
+def changeValue(key,value):
+    sessionData[sessID][key]=value
 for key in fromClient:
     if key != 'sessID':
         sessionData[sessID][key]=fromClient[key].value
@@ -39,14 +42,35 @@ Silent Saw,
 Increased health <br>
     <input type="radio" name="'''+str(player)+'''Class" value="mastermind"> Mastermind: Health Kit,
 Intimidation,
-Infinite Cable Ties<br>
+Inspire<br>
     <input type="radio" name="'''+str(player)+'''Class" value="ghost"> Ghost: EMC Jammer,
 Silenced Weapon,
 Lockpick without drill<br>'''+
     htmlFunctions.element(False,1,'input','type=submit',"")
     )
-    sessionData[sessID]['player']=nextPlayer()
+    changeValue('player',nextPlayer())
+elif sessionData[sessID]['initiated']==False:
+    for player in playerList:
+        if player+"Class"=="enforcer":
+            changeValue(player+"Health",6)
+        else:
+            changeValue(player+'Health',3)
+        changeValue(player+'Ammo',6)
+    changeValue('',)
+    changeValue('',)
+    changeValue('',)
+    changeValue('',)
+    changeValue('',)
+    changeValue('',)
+    changeValue('',)
+    changeValue('',)
+    changeValue('',)
+    changeValue('',)
+    changeValue('',)
+    changeValue('',)
+    changeValue('',)
+    changeValue('initiated',True)
+
 print htmlFunctions.bodySetup(output)
 print htmlFunctions.html_end()
-print sessionData
 dictToCsv.dictToCsv(sessionData)
